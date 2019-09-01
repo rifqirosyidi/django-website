9dfd87f6f11c9c9ec31ae08d0970bd70598c70d2
Rifqi Rosyidi <rief.rosyidi@gmail.com>
2019-09-01 17:47:45 +0700

    get object or 404 and remove hardcoded urls

--- music/urls.py
+++ music/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+app_name = 'music'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:album_id>/', views.detail, name='detail'),
--- music/views.py
+++ music/views.py
@@ -1,5 +1,4 @@
-from django.http import HttpResponse, Http404
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Album, Song
 
 
@@ -11,9 +10,6 @@ def index(request):
 
 
 def detail(request, album_id):
-    try:
-        album = Album.objects.get(pk = album_id)
-    except Album.DoesNotExist:
-        raise Http404("Album dont Exist")
+    album = get_object_or_404(Album, pk=album_id)
     return render(request, 'music/detail.html', {'album': album})
 
