101a4c12d6822d51f71f5e044edba0c0713c59fc
Rifqi Rosyidi <rief.rosyidi@gmail.com>
2019-09-02 12:01:46 +0700

    using generic view

--- music/urls.py
+++ music/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from . import views
+from .import views
 
 app_name = 'music'
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('<int:album_id>/', views.detail, name='detail'),
-    path('<int:album_id>/favorite', views.favorite, name='favorite'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
 ]
--- music/views.py
+++ music/views.py
@@ -1,29 +1,15 @@
-from django.shortcuts import render, get_object_or_404
+from django.views import generic
 from .models import Album, Song
 
 
-# Create your views here.
-def index(request):
-    all_albums = Album.objects.all()
-    context = {'all_albums': all_albums}
-    return render(request, 'music/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'music/index.html'
+    context_object_name = 'album_list'
+
+    def get_queryset(self):
+        return Album.objects.all()
 
 
-def detail(request, album_id):
-    album = get_object_or_404(Album, pk=album_id)
-    return render(request, 'music/detail.html', {'album': album})
-
-
-def favorite(request, album_id):
-    album = get_object_or_404(Album, pk=album_id)
-    try:
-        selected_song = album.song_set.get(pk=request.POST['song'])
-    except (KeyError, Song.DoesNotExist):
-        return render(request, 'music/detail.html', {
-            'album': album,
-            'error_message': 'You didn\'t select a valid song'
-        })
-    else:
-        selected_song.is_favorite = True
-        selected_song.save()
-        return render(request, 'music/detail.html', {'album': album})
+class DetailView(generic.DetailView):
+    model = Album
+    template_name = 'music/detail.html'
