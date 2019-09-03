b5f7444989b42d805c563957aaded7e9d1bd28fa
Rifqi Rosyidi <rief.rosyidi@gmail.com>
2019-09-03 14:11:02 +0700

    registration

--- music/urls.py
+++ music/urls.py
@@ -3,6 +3,8 @@ from .import views
 
 app_name = 'music'
 urlpatterns = [
+    path('register/', views.UserFormView.as_view(), name='register'),
+
     path('', views.IndexView.as_view(), name='index'),
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     path('album/add', views.AlbumCreate.as_view(), name='album-create'),
